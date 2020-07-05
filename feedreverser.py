11300c91413e8a02e28fad89bdd11e2aa8a18a73
Steinar Bang <sb@dod.no>
2020-07-05 08:28:25 +0100

    Remove "." from terms to avoid hashtags terminating early

--- feedreverser.py
+++ feedreverser.py
@@ -30,7 +30,7 @@ def main():
             fe.summary(summarySoup.get_text())
             for tag in entry.tags:
                 if 'uncategorized' != tag.term.lower():
-                    fe.category(term = tag.term.replace(' ', '_'))
+                    fe.category(term = tag.term.replace(' ', '_').replace('.', ''))
             now = datetime.now(tz=timezone.utc)
             fe.pubDate(now)
             fe.updated(now)
